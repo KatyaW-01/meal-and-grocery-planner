a7752daec10281fb8e51b8813c9e5b2e168ce913
Katya Weicht <katya.weicht@gmail.com>
2025-10-24 14:03:57 -0600

    Make functions for creating a recipe and ingredients

--- client/src/components/SaveRecipeForm.jsx
+++ client/src/components/SaveRecipeForm.jsx
@@ -2,21 +2,50 @@ import { scrape } from "../api/firecrawl"
 import { useState } from "react"
 import { createRecipe } from "../api/recipes"
 import { addIngredient } from "../api/ingredients"
+import {UserContext} from '../UserContext'
+import {useContext} from "react"
 import '../styles/recipeIngredientForms.css'
 
 function SaveRecipeForm() {
+  const { user, setUser } = useContext(UserContext)
   const [url, setUrl] = useState("")
   const [date, setDate] = useState("")
   const [recipeData, setRecipeData] = useState(null)
 
   async function handleSubmit(event) {
     event.preventDefault()
+    if(!date) {
+      alert("Please enter a date")
+    }
+    if(!url) {
+      alert("Error url cannot be blank")
+    }
     const result = await scrape(url)
     console.log("result:",result)
-    setRecipeData(result)
-    //add recipe data to the database
-    //recipe has title, instructions, date, and userId
-    //ingredients have name, quantity, quantity_description, recipe_id
+    const recipeObj = await createRecipeObject(result)
+    console.log("recipe object:",recipeObj)
+    if(recipeObj) {
+      await createIngredients(recipeObj)
+    }
+  }
+  
+  async function createRecipeObject(data) {
+    if(data && date) {
+      //create recipe object
+      const newRecipe = await createRecipe({title: data.title, instructions: (data.instructions).join(" "), date: date })
+      //update state if recipe created successfully
+      if(!newRecipe.error) {
+        setUser(prev => ({
+          ...prev, recipes:[...prev.recipes, newRecipe]
+        }))
+
+        return newRecipe
+      }
+    } 
+  }
+
+  async function createIngredients(recipe) {
+    console.log("recipeId:", recipe.id)
   }
 
   function handleChange(event) {
@@ -26,15 +55,7 @@ function SaveRecipeForm() {
     }
     if(name==='date') {
       setDate(value)
-    }
-  }
-
-  async function addRecipe() {
-    await createRecipe({title: recipeData.title, instructions: recipeData.instructions.join(" "), date: date })
-  }
-
-  if(recipeData && date) {
-    addRecipe()
+    } 
   }
 
   return (
