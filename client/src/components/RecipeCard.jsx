d534a10e7eff695745546f0f8f11e53bf10801f7
Katya Weicht <katya.weicht@gmail.com>
2025-11-04 16:20:41 -0700

    Display ingredients and insturctions on recipe modal

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -4,21 +4,25 @@ import RecipeModal from "./RecipeModal"
 
 function RecipeCard({recipe}) {
   const [recipeClick, setRecipeClick] = useState(false)
+  const [recipeData, setRecipeData] = useState(recipe)
+ 
   function handleClick() {
     setRecipeClick(true)
   }
+
   function closeModal() {
     setRecipeClick(false)
   }
+  
   return (
     <div className='recipe-card'>
       <h3>{recipe.title}</h3>
       <button className='more-info-button' onClick={handleClick}>More Info</button>
       {recipeClick && (
-        <> 
+        <div> 
           <div className='modal-overlay'> </div>
-          <RecipeModal recipe={recipe} onClose={closeModal}/>
-        </>
+          <RecipeModal recipe={recipeData} onClose={closeModal}/>
+        </div>
       )}
     </div>
   )
--- client/src/components/RecipeModal.jsx
+++ client/src/components/RecipeModal.jsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from "react"
+
 function RecipeModal({recipe, onClose}) {
+  const [ingredients, setIngredients] = useState([])
+  console.log(recipe)
+  useEffect(() => {
+    let ingredientArray = []
+    recipe.ingredients.map((ingredient) => {
+      ingredientArray.push(`${ingredient.name} (${ingredient.quantity} ${ingredient.quantity_description})`)
+    })
+    setIngredients(ingredientArray)
+  },[])
   return (
     <div className='event-modal'>
       <button className='x-button' onClick={onClose}>X</button>
-      <p>recipe information here</p>
+      <p>{recipe.title}</p>
+      <p>Ingredients: {ingredients.join(', ')}</p>
+      <p>Instructions: {recipe.instructions}</p>
+      <button>Add Recipe to Calendar</button>
     </div>
   )
 }
